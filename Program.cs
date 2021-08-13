a9e2d0909431ddce85de70374f5fead6a2dc8dd1
fluxehub <53355311+fluxehub@users.noreply.github.com>
2021-08-14 00:02:42 +0100

    Put code into a class to make it a bit neater

--- Program.cs
+++ Program.cs
@@ -1,48 +1,60 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
 using Microsoft.Extensions.Logging;
-using StormWatch;
 
-var config = Config.FromYaml("./config.yml");
-
-var discord = new DiscordClient(new DiscordConfiguration()
+namespace StormWatch
 {
-    Token = config.Token,
-    TokenType = TokenType.Bot,
-    Intents = DiscordIntents.All
-});
-
-await discord.ConnectAsync();
-
-discord.Ready += (sender, _) =>
-{
-    sender.Logger.LogInformation($"Connected to Discord as {discord.CurrentUser.Username}#{discord.CurrentUser.Discriminator}");
-    return Task.CompletedTask;
-};
-
-static bool IsOnline(UserStatus status) =>
-    status is not (UserStatus.Offline or UserStatus.Invisible);
-
-discord.PresenceUpdated += async (sender, args) =>
-{
-    var wasOnline = IsOnline(args.PresenceBefore.Status);
-    var isOnline = IsOnline(args.PresenceAfter.Status);
-
-    if (args.User.Id != config.User || wasOnline == isOnline)
-        return;
-
-    var channel = await sender.GetChannelAsync(config.Channel);
-    
-    if (isOnline)
+    public class Program
     {
-        await channel.SendMessageAsync(config.Gifs.Online);
-    }
-    else
-    {
-        await channel.SendMessageAsync(config.Gifs.Offline);
-    }
-};
+        private static Config _config;
 
-await Task.Delay(-1);
+        private static bool IsOnline(UserStatus status) =>
+            status is not (UserStatus.Offline or UserStatus.Invisible);
+
+        private static async Task PostOnlineStatusChange(DiscordClient sender, PresenceUpdateEventArgs args)
+        {
+            var wasOnline = IsOnline(args.PresenceBefore.Status);
+            var isOnline = IsOnline(args.PresenceAfter.Status);
+
+            if (args.User.Id != _config.User || wasOnline == isOnline)
+                return;
+
+            var channel = await sender.GetChannelAsync(_config.Channel);
+
+            if (isOnline)
+            {
+                await channel.SendMessageAsync(_config.Gifs.Online);
+            }
+            else
+            {
+                await channel.SendMessageAsync(_config.Gifs.Offline);
+            }
+        }
+
+        public static async Task Main()
+        {
+            _config = Config.FromYaml("./config.yml");
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = _config.Token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All
+            });
+
+            discord.Ready += (sender, _) =>
+            {
+                sender.Logger.LogInformation(
+                    $"Connected to Discord as {discord.CurrentUser.Username}#{discord.CurrentUser.Discriminator}");
+                return Task.CompletedTask;
+            };
+
+            discord.PresenceUpdated += PostOnlineStatusChange;
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
